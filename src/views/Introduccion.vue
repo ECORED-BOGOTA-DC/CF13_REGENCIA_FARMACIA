4cf5d18318c1703af5173df9f359135372cbaabc
Jhon Edinson Castañeda Oviedo <48686871+JhonEdinson-Shongo@users.noreply.github.com>
2023-08-29 08:45:23 -0500

    Ajuste Diseño

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -19,7 +19,10 @@
           br
           |Se encuentran reglamentados por la resolución 1403 del 2007 y aunque ambos funcionan de manera diferente, tienen una particularidad y es que parten de una prescripción médica otorgada por un médico a un paciente. La entrega de los productos farmacéuticos prescritos se realiza directamente al paciente por medio del proceso de dispensación y a la enfermera responsable de su administración por medio del proceso de distribución. Así mismo cabe mencionar que la efectividad en la terapia medicamentosa de un paciente también está ligada a la correcta administración de los medicamentos.
 
-    p.mb-4(data-aos="fade-right") A continuación, se abordarán diferentes temas relacionados con ambos procesos que permitirán al lector ahondar en las particularidades de cada uno de ellos y comprender que tanto la dispensación como la distribución son actos profesionales propios de los farmacéuticos y son el origen del uso racional de los medicamentos. También se abordarán algunos puntos importantes de la administración de medicamentos que van unidos a toda la información anteriormente estudiada.
+    p.mb-5(data-aos="fade-right") A continuación, se abordarán diferentes temas relacionados con ambos procesos que permitirán al lector ahondar en las particularidades de cada uno de ellos y comprender que tanto la dispensación como la distribución son actos profesionales propios de los farmacéuticos y son el origen del uso racional de los medicamentos. También se abordarán algunos puntos importantes de la administración de medicamentos que van unidos a toda la información anteriormente estudiada.
+    figure.mb-5
+      .video
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
     p.text-center.fw-bold ¡Muchos éxitos en este proceso de aprendizaje!
 </template>
 
